cc006036ada3a2840469fbf641bdd5c5dadc8d4e
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-02-25 14:36:35 +0000

    can touch in, touch out, be in journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,31 @@
 class Oystercard
+  DEFAULT_BALANCE = 0
   MAX_BALANCE = 90
-  attr_accessor :balance
+  attr_accessor :balance, :MAX_BALANCE
 
-  def initialize
-    @balance = 0
+  def initialize(balance = DEFAULT_BALANCE)
+    fail "Maximum balance is #{Oystercard::MAX_BALANCE}" if balance > MAX_BALANCE
+    @balance = balance
   end
 
   def top_up(money)
-    fail "Maximum balance is £90" if money + balance > MAX_BALANCE
+    fail "Maximum balance is #{Oystercard::MAX_BALANCE}" if money + balance > MAX_BALANCE
     @balance += money
   end
 
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,9 +11,29 @@ describe Oystercard do
     expect(card.top_up(10)).to eq(10)
   end
 
-  it "raises error if balance exceeds £90" do
-    card = Oystercard.new
-    card.balance = 90
-    expect {card.top_up(0.01) }.to raise_error "Maximum balance is £90"
+  it "raises error if balance exceeds MAX_BALANCE" do
+    card = Oystercard.new(90)
+    expect {card.top_up(0.01) }.to raise_error "Maximum balance is #{Oystercard::MAX_BALANCE}"
   end
+
+  it "deducts money from the balance when fare is taken" do
+    card = Oystercard.new(10)
+    expect(card.deduct(4)).to eq(6)
+  end
+
+  it "is initially not in use" do
+    expect(subject).not_to be_in_journey
+  end
+
+  it "responds to touch_in" do
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+
+  it "responds to touch_out" do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
+  
 end
